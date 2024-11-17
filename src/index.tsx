1ff9d3f5298c49e7f5f053a6f941b917437c0b7c
MosabDwaikat <mosabdwaikat@gmail.com>
2024-11-17 18:05:17 +0200

    fix: strictMode readded

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ import router from "./router";
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
 root.render(
-  <>
+  <React.StrictMode>
     <Provider store={store}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
@@ -23,7 +23,7 @@ root.render(
         />
       </ThemeProvider>
     </Provider>
-  </>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
